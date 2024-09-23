ac452bfab25fd094f0172b58c0024985cda7f3af
Passawee Sombunwanich <bkbase456@gmail.com>
2024-09-23 16:56:06 +0700

    Create Connect_Four class with display_board and  player_play method.

--- Computer_Science_Course/Python_Terminal_Game/Connect_Four.py
+++ Computer_Science_Course/Python_Terminal_Game/Connect_Four.py
@@ -1,1 +1,46 @@
 print("This is Connect Four Game")
+
+class Connect_Four:
+    player1_symbol = 'X'
+    player2_symbol = 'O'
+
+    def __init__(self):
+        self.board = [[None for _ in range(7)] for _ in range(6)]
+        self.is_player1 = True
+    
+    def display_board(self):
+        text_length = 0
+        for column in self.board:
+            row_text = "| "
+            for row in column:
+                if row is None:
+                    row_text += "  | "
+                else:
+                    row_text += f"{row}" + " | "
+            
+            print(row_text)
+            text_length = len(row_text)
+        print("-" * text_length)
+                
+    
+    def player_play(self, column):
+        row = 5
+        while (self.board[row][column] != None):
+            row -= 1
+            if row < 0:
+                print("This column is full, please play other column.")
+                return
+                
+        if self.is_player1:
+            self.board[row][column] = Connect_Four.player1_symbol
+            self.is_player1 = False
+        else:
+            self.board[row][column] = Connect_Four.player2_symbol
+            self.is_player1 = True
+
+game = Connect_Four()
+game.display_board()
+game.player_play(0)
+game.display_board()
+game.player_play(0)
+game.display_board()
